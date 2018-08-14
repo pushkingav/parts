befcb176da5d12bd7e2f3177f06cd963ac437737
apushkin <apushkin@ipmks.com>
2018-08-14 18:34:14 +0300

    Started to add UI features.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NoteComponent} from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/note/note.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {Note} from '../models/user.model';
+import {NoteService} from '../note.service';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent implements OnInit {
+  notes: Note[];
+  constructor(private router: Router, private noteService: NoteService ) {  }
+
+  ngOnInit() {
+    this.noteService.getNotes()
+      .subscribe( data => {
+        this.notes = data;
+      });
+  }
+  deleteNote(note: Note): void {
+    this.noteService.deleteNote(note)
+      .subscribe( data => {
+        this.notes = this.notes.filter(n => n !== note);
+      });
+  }
+}
